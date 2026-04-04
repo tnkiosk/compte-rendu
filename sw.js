87b80b475df0f51ab59f489902d95879839ffeba
tnkiosk <attiagermain@gmail.com>
2026-04-04 17:27:45 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,16 +7,37 @@ const URLS_TO_CACHE = [
   '/compte-rendu/manifest.webmanifest'
 ];
 
+// Installation
 self.addEventListener('install', (event) => {
+  self.skipWaiting(); // force la mise à jour
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(URLS_TO_CACHE))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
   );
 });
 
+// Activation (supprime anciens caches)
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim(); // prend le contrôle direct
+});
+
+// Requête
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
