52652ad32520584d04a5c26152bcb00da0eaecaf
tnkiosk <attiagermain@gmail.com>
2026-04-04 18:27:09 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'compte-rendu-v5';
+const CACHE_NAME = 'compte-rendu-v7';
 
 const URLS_TO_CACHE = [
   '/compte-rendu/',
@@ -7,44 +7,45 @@ const URLS_TO_CACHE = [
   '/compte-rendu/manifest.webmanifest'
 ];
 
+// Installation
 self.addEventListener('install', (event) => {
   self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(URLS_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
   );
 });
 
+// Activation (nettoyage anciens caches)
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
         cacheNames.map((cacheName) => {
           if (cacheName !== CACHE_NAME) {
             return caches.delete(cacheName);
           }
         })
-      );
-    }).then(() => self.clients.claim())
+      )
+    ).then(() => self.clients.claim())
   );
 });
 
+// Requêtes (toujours version la plus récente)
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return;
 
   event.respondWith(
     fetch(event.request)
       .then((response) => {
-        const responseClone = response.clone();
+        const clone = response.clone();
         caches.open(CACHE_NAME).then((cache) => {
-          cache.put(event.request, responseClone);
+          cache.put(event.request, clone);
         });
         return response;
       })
       .catch(() => {
-        return caches.match(event.request).then((response) => {
-          return response || caches.match('/compte-rendu/index.html');
+        return caches.match(event.request).then((res) => {
+          return res || caches.match('/compte-rendu/index.html');
         });
       })
   );
